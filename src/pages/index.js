a1ff63af4ed921f223a9c346bf4a725141d84782
Stephen Puthenpurackal <datfoosteve@gmail.com>
2022-12-15 07:24:47 -0600

    im getting tired

---
+++ src/pages/index.js
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+import React, { useEffect, useState } from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import About from '../components/About'
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import Navigation from './navigation';
+import { motion } from "framer-motion";
+
+export default function Home() {
+  const [resumeData, setResumeData] = useState({})
+
+  useEffect(() => {
+    fetch('/resumeData.json')
+      .then((res) => res.json())
+      .then((data) => {
+        setResumeData(data)
+      })
+  }, [])
+  return (
+    <div className="mx-auto flex w-full flex-col items-center justify-center">
+      <Head>
+        <title>twonarly</title>
+        <link rel="icon" href="/two.png" />
+      </Head>
+      <Header data={resumeData.main} />
+      <About data={resumeData.main} />
+      <Contact data={resumeData.main} />
+      <Footer data={resumeData.main} />
+    </div>
+  )
+}
