20c22af61939940397ba03ec5406fe3ac03da355
Stephen Puthenpurackal <datfoosteve@gmail.com>
2022-12-17 20:24:54 -0600

    dam dude

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,14 +3,26 @@ import '../styles/globals.css'
 import Layout from '../components/Layout'
 import { ParallaxProvider } from 'react-scroll-parallax';
 
+
 function MyApp({ Component, pageProps }) {
   return (
     <ParallaxProvider>
   <Layout>
     <Component {...pageProps} />
   </Layout>
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.section}>
+        <Text>Section #1</Text>
+      </View>
+      <View style={styles.section}>
+        <Text>Section #2</Text>
+      </View>
+    </Page>
+  </Document>
   </ParallaxProvider>
+
   )
-}
+}   
 
 export default MyApp
