149cef5acd7135f3e8c8037ffe59eb54762d8d7a
Stephen Puthenpurackal <datfoosteve@gmail.com>
2022-12-23 19:41:07 -0600

    okay

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,9 +1,13 @@
+
+import Resume from './path/to/Resume';
+
 import React from 'react'
 // Import the main component
 import { Viewer } from '@react-pdf-viewer/core';
 
 // Import the styles
 import '@react-pdf-viewer/core/lib/styles/index.css';
+
 
 
 
