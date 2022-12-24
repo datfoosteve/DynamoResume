2cecea14ece50be1bf806c00e4775023b7c25bd8
Stephen Puthenpurackal <datfoosteve@gmail.com>
2022-12-23 19:44:05 -0600

    okay here

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,8 @@ import Contact from '../components/Contact'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Portfolio from '../components/Portfolio'
-import resumeData from resumeData 
+import resumeData from '../resumeData'
+import PDFresume from './PDFresume';
 import Testimonials from '../components/Testimonials'
 import Resume from './components/Resume';
 import '@react-pdf-viewer/core/lib/styles/index.css'
@@ -22,7 +23,7 @@ export default function Home() {
   <Header data={resumeData.main} />
   <About data={resumeData.main} />
   <Resume data={resumeData.resume} />
-  
+  <PDFresume data={resumeData.main} />
   <Portfolio data={resumeData.portfolio} />
   <Testimonials data={resumeData.testimonials} />
   <Contact data={resumeData.main} />
